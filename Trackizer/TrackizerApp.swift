0c6b7f224650391e5f06627c0f46e165ab050a86
codeforany <130132429+codeforany@users.noreply.github.com>
2023-07-11 19:31:10 +0530

    Initial Commit

---
+++ Trackizer/TrackizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackizerApp.swift
+//  Trackizer
+//
+//  Created by CodeForAny on 11/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TrackizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
